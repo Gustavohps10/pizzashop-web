42de6fa872764fd7706f9a339061f75ebbdbb7c9
Gustavo Henrque <gustavoh.santos735@gmail.com>
2024-06-30 09:05:54 -0300

    chore: add eslint config

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import './global.css'
-import { Button } from "@/components/ui/button"
 
-export function App(){
+import { Button } from '@/components/ui/button'
+
+export function App() {
   return (
-    <div className='flex justify-center items-center h-screen'>
-    <Button>Click me</Button>
+    <div className="flex h-screen items-center justify-center">
+      <Button>Click me</Button>
     </div>
   )
 }
