f0f2a7eaa2683407c80a136e41b51dea7b06813a
Gustavo Henrque <gustavoh.santos735@gmail.com>
2024-07-01 10:13:47 -0300

    feat: add account dropdown meno in header

---
+++ src/components/account-menu.tsx
@@ -0,0 +1,46 @@
+import { Building, ChevronDown, LogOut } from 'lucide-react'
+
+import { Button } from './ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from './ui/dropdown-menu'
+
+export function AccountMenu() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant="outline"
+          className="flex select-none items-center gap-2"
+        >
+          Estabelecimento
+          <ChevronDown className="h4 w-4" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end" className="w-56">
+        <DropdownMenuLabel className="flex flex-col">
+          <span>Manager Name</span>
+          <span className="text-sm font-normal text-muted-foreground">
+            example@gmail.com
+          </span>
+        </DropdownMenuLabel>
+
+        <DropdownMenuSeparator />
+
+        <DropdownMenuItem>
+          <Building className="mr-2 h-4 w-4" />
+          <span>Perfil das lojas</span>
+        </DropdownMenuItem>
+        <DropdownMenuItem className="cursor-pointer text-rose-500 dark:text-rose-400">
+          <LogOut className="mr-2 h-4 w-4" />
+          <span>Sair</span>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
