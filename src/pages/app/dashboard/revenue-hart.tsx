2836cc77da22cc04e6ba7d684a0f6e833d17b503
Gustavo Henrque <gustavoh.santos735@gmail.com>
2024-07-02 20:47:41 -0300

    feat(dashboard): add revenue chart

---
+++ src/pages/app/dashboard/revenue-hart.tsx
@@ -0,0 +1,70 @@
+import {
+  CartesianGrid,
+  Line,
+  LineChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from 'recharts'
+import colors from 'tailwindcss/colors'
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+
+const data = [
+  { date: '10/12', revenue: 1200 },
+  { date: '11/12', revenue: 1300 },
+  { date: '12/12', revenue: 400 },
+  { date: '13/12', revenue: 800 },
+  { date: '14/12', revenue: 1600 },
+  { date: '15/12', revenue: 1700 },
+  { date: '16/12', revenue: 890 },
+]
+
+export function RevenueChart() {
+  return (
+    <Card className="col-span-6">
+      <CardHeader className="flex-row items-center justify-between pb-8">
+        <div className="space-y-1">
+          <CardTitle className="text-base font-medium">
+            Receita no periodo
+          </CardTitle>
+          <CardDescription>Receita diária no periodo</CardDescription>
+        </div>
+      </CardHeader>
+      <CardContent>
+        <ResponsiveContainer width="100%" height={240}>
+          <LineChart data={data} style={{ fontSize: 12 }}>
+            <XAxis dataKey="date" axisLine={false} tickLine={false} dy={16} />
+
+            <YAxis
+              stroke="#888"
+              axisLine={false}
+              tickLine={false}
+              width={80}
+              tickFormatter={(value: number) =>
+                value.toLocaleString('pt-BR', {
+                  style: 'currency',
+                  currency: 'BRL',
+                })
+              }
+            />
+
+            <Line
+              type="linear"
+              strokeWidth={2}
+              dataKey="revenue"
+              stroke={colors.rose['500']}
+            />
+          </LineChart>
+        </ResponsiveContainer>
+      </CardContent>
+    </Card>
+  )
+}
