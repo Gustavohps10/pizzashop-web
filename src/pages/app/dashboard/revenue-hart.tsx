6665179d7bd057d9686c51781e7e1345f3028815
Gustavo Henrque <gustavoh.santos735@gmail.com>
2024-07-02 20:51:35 -0300

    feat(dashboard): add cartesian grid in revenue chart

--- src/pages/app/dashboard/revenue-hart.tsx
+++ src/pages/app/dashboard/revenue-hart.tsx
@@ -56,6 +56,8 @@ export function RevenueChart() {
               }
             />
 
+            <CartesianGrid vertical={false} className="stroke-muted" />
+
             <Line
               type="linear"
               strokeWidth={2}
