2be6b0f43996438c290fd20009072e3d2d6c74a5
Gustavo Henrque <gustavoh.santos735@gmail.com>
2024-07-06 20:54:44 -0300

    feat: add error page

---
+++ src/pages/error.tsx
@@ -0,0 +1,17 @@
+import { Link } from 'react-router-dom'
+
+export function Error() {
+  //   const error = useRouteError()
+
+  return (
+    <div className="flex h-screen flex-col items-center justify-center gap-2">
+      <h1 className="text-4xl font-bold">Ooops! Algo errado aconteceu</h1>
+      <p className="text-accent-foreground">
+        Voltar para o{' '}
+        <Link to="/" className="text-sky-600 dark:text-sky-400">
+          Dashboard
+        </Link>
+      </p>
+    </div>
+  )
+}
