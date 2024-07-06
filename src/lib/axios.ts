0e06f2a057339e011c28376ad3a5caf8c6e2f784
Gustavo Henrque <gustavoh.santos735@gmail.com>
2024-07-06 19:45:29 -0300

    chore: add random delay to api requests

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -9,7 +9,9 @@ export const api = axios.create({
 
 if (env.VITE_ENABLE_API_DELAY) {
   api.interceptors.request.use(async (config) => {
-    await new Promise((resolve) => setTimeout(resolve, 2000))
+    await new Promise((resolve) =>
+      setTimeout(resolve, Math.round(Math.random() * 2000)),
+    )
     return config
   })
 }
