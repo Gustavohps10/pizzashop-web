2a6729fac7e820078f7ecf3e99e31d0a960dd4f2
Gustavo Henrique <gustavoh.santos735@gmail.com>
2024-07-08 18:53:19 -0300

    test: create unit test for pagination component

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,15 @@
+import path from 'node:path'
+
 import react from '@vitejs/plugin-react'
 import { defineConfig, UserConfig } from 'vitest/config'
 
 export default defineConfig({
   plugins: [react()] as UserConfig['plugins'],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   test: {
     globals: true,
     setupFiles: ['./test/setup.ts'],
