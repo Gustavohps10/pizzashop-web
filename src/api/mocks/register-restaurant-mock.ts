2bce9d34d57d528115f580726ad9d32f5b7834ff
Gustavo Henrique <gustavoh.santos735@gmail.com>
2024-07-09 13:48:16 -0300

    test: add register restaurant mock

---
+++ src/api/mocks/register-restaurant-mock.ts
@@ -0,0 +1,20 @@
+import { http, HttpResponse } from 'msw'
+
+import { RegisterRestaurantBody } from '../register-restaurant'
+
+export const registerRestaurantMock = http.post<never, RegisterRestaurantBody>(
+  '/restaurants',
+  async ({ request }) => {
+    const { restaurantName } = await request.json()
+
+    if (restaurantName === 'Pizza Shop') {
+      return new HttpResponse(null, {
+        status: 201,
+        headers: {
+          'Set-Cookie': 'auth=sample-jwt',
+        },
+      })
+    }
+    return new HttpResponse(null, { status: 400 })
+  },
+)
